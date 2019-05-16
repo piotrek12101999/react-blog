4938fcf3534b449644ef3b6b01602dee01d28179
Piotr <piter.gazownik@o2.pl>
2019-05-16 12:59:17 +0200

    Done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PostsList from './components/PostsList';
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <PostsList />
+    </div>
+  );
+};
+
+export default App;
